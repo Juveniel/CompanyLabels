ba86edad7d08b8707d2d029c64aecc49c93a76d8
Dimitar Kalaydzhiev <dimitar.kalaydzhievv@gmail.com>
2016-07-16 19:02:38 +0300

    Added project skeleton, base migrations and settings

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('first_name', 40)->nullable();
+            $table->string('last_name', 40)->nullable();
+            $table->string('country', 40)->nullable();
+            $table->string('city', 40)->nullable();
+            $table->string('avatar');
+            $table->string('facebook');
+            $table->string('linked_in');
+            $table->string('google_plus');
+            $table->rememberToken();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('users');
+    }
+}
---
+++ database/migrations/2016_07_16_153153_create_countries_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCountriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('countries', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->string('code', 3)->unique();
+            $table->string('name', 52);
+            $table->enum('continent', ['Asia', 'Europe', 'North America', 'Africa', 'Oceania', 'Antarctica', 'South America']);
+            $table->integer('capital')->length(11)->unsigned();
+            $table->string('code2', 2);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('countries');
+    }
+}
