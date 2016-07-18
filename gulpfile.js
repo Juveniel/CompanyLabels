1b2a3caddcd102cf9552f8321f0744d5727d4641
Dimitar Kalaydzhiev <dimitar.kalaydzhievv@gmail.com>
2016-07-18 23:50:16 +0300

    Added basic resources task automation

--- gulpfile.js
+++ gulpfile.js
@@ -12,5 +12,31 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
+    //bower components
+    var bower_folder = "vendor/bower_dl/";
+
+    //bower css
+    mix.copy(bower_folder + 'bootstrap/dist/css/bootstrap.css', 'public/css/bootstrap.css');
+
+    //bower js
+    mix.copy(bower_folder + 'jquery/dist/jquery.js', 'public/js/jquery.js');
+    mix.copy(bower_folder + 'bootstrap/dist/js/bootstrap.js', 'public/js/bootstrap.js');
+
+    //mix css
+    mix.styles([
+        'bootstrap.css',
+        'app.css'
+    ], 'public/output/front.css', 'public/css');
+
+    //mix sass
     mix.sass('app.scss');
+
+    //mix scripts
+    mix.scripts([
+        'jquery.js',
+        'bootstrap.js',
+        'front_main.js'
+    ], 'public/output/front.js', 'public/js');
+
+    mix.version(["public/output/front.css", "public/output/front.js"]);
 });
