8c31f30dc68adea4d9b9fba73c6d363bb5105fea
Dimitar Kalaydzhiev <dimitar.kalaydzhievv@gmail.com>
2016-09-11 08:10:21 +0300

    Added company migration tables

---
+++ database/migrations/2016_09_11_044150_create_company_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCompanyTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->nullable();
+            $table->string('email')->unique();
+            $table->string('address')->nullable();
+            $table->string('bulstat')->nullable();
+            $table->string('mol')->nullable();
+            $table->string('description')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('companies');
+    }
+}
